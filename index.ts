945ca97d6dd15dbbdb0aa6dc99750aab56b57687
Sanyam Lohan <sanyamlohan@gmail.com>
2024-05-04 01:48:22 +0530

    created input utility to take input from terminal

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("hello world");
+import { TPlayer } from "./player";
+import { cin } from "./utils";
+
+const init = async () => {
+    const player1: TPlayer = {
+        health: 0,
+        strength: 0,
+        attack: 0
+    }
+    player1.health = +(await cin("Health of player 1: "));
+    console.log("yay! i got it: ", JSON.stringify(player1, null, 2));
+};
+
+init();
--- utils.ts
+++ utils.ts
@@ -1,3 +1,16 @@
+import { createInterface } from "node:readline/promises";
+
 // function to get a random number between min and max
 export const random = (min: number, max: number) =>
     Math.floor(Math.random() * (max - min + 1)) + min;
+
+// take input from terminal
+export const cin = async (question: string = "") => {
+    const readline = createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    const answer = await readline.question(question);
+    readline.close();
+    return answer;
+};
